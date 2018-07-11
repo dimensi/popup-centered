caf7564c58350753184cf93b947e45c091dfba22
Nikita Nafranets <eddimensi@gmail.com>
2018-07-11 13:42:26 +0300

    add dist in ignore and remove dist from git

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { WindowFeaturesOptions } from './utils';
-declare function popupCentered(url: string, title: string, width: number, height: number): Window;
-declare function popupCentered(url: string, title: string, options: WindowFeaturesOptions): Window;
-export default popupCentered;
