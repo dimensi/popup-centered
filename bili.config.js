b4eef879281652d9fd990a8a81a1be9704ddc59f
Nikita Nafranets <eddimensi@gmail.com>
2018-07-11 12:34:35 +0300

    update bili config

--- bili.config.js
+++ bili.config.js
@@ -2,11 +2,11 @@ module.exports = {
   input: 'src/index.ts',
   format: ['umd', 'es', 'iife-min'],
   banner: true,
-  moduleName: 'PopupCentered',
+  moduleName: 'popupCentered',
   plugin: [
     require('rollup-plugin-clear')({
       targets: ['./dist/']
-    })
+    }),
   ],
   exports: 'named'
 }
